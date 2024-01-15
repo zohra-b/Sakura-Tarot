19f31e116bf5cdbb238f454fecaa135a823fab76
Jasz-17 <jaszapata1983@gmail.com>
2024-01-15 13:13:53 +0100

    importacion libreria carousel

--- src/app/components/Carousel/Carousel.jsx
+++ src/app/components/Carousel/Carousel.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
 
-export default function Carousel({ cards }) {
+import Slider from "react-slick";
+import React, {useState, useEffect} from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+/* export default function Carousel({ cards }) {
   return (
     <div>
       {cards.map((card) => (
         <p key={card.id}>{card.spanishName}</p>
       ))}
     </div>
+  );
+} */
+
+export default function Carousel() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
   );
 }
