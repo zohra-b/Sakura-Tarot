f7da1befb2355e3f650d860bf3b39fb46f34fc2a
fefy <stebofof@gmail.com>
2024-01-19 19:37:29 +0100

    Añadido input y estilos a home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 import Form from './components/Form/Form';
-import Button from './components/Button/Button';
 import styles from './page.module.css';
 import { useCards } from '@/context/cardContext';
 
@@ -8,19 +7,18 @@ export default function Home() {
   const cards = useCards();
   return (
     <main className={styles.main}>
-     <section className='section'> 
-        <h1>
-          <img className={styles.sakuraOrnament}src="/img/sakura-ornament.png" alt="flor sakura" />
+      <section className={styles.section}>
+        <h1 className={styles.titleWithOrnament}>
+          <img className={styles.sakuraOrnament} src="/img/sakura-ornament.png" alt="flor sakura" />
           Bienvenida a Sakura Tarot
           <img className={styles.sakuraOrnament} src="/img/sakura-ornament.png" alt="flor sakura" />
         </h1>
-        
+
         <h2>Deja que nuestras cartas te guien</h2>
-        <Form/>
-        <Button primary text={'Ir a la tirada'}/>
+        <Form />
       </section>
-      <aside>
-        <img  className={styles.heroImg} src="/img/hero-img.png" alt="imagen tres cartas" />
+      <aside className={styles.aside}>
+        <img className={styles.heroImg} src="/img/hero-img.png" alt="imagen tres cartas" />
       </aside>
     </main>
   );
