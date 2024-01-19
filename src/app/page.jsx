abe1bca54172fe2d9cd76078bca83da0aaff93a7
zohra-b <150009936+zohra-b@users.noreply.github.com>
2024-01-19 17:51:09 +0100

    creado componente button y estilos homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,6 @@
 'use client';
+import Form from './components/Form/Form';
+import Button from './components/Button/Button';
 import styles from './page.module.css';
 import { useCards } from '@/context/cardContext';
 
@@ -6,6 +8,20 @@ export default function Home() {
   const cards = useCards();
   return (
     <main className={styles.main}>
+     <section className='section'> 
+        <h1>
+          <img className={styles.sakuraOrnament}src="/img/sakura-ornament.png" alt="flor sakura" />
+          Bienvenida a Sakura Tarot
+          <img className={styles.sakuraOrnament} src="/img/sakura-ornament.png" alt="flor sakura" />
+        </h1>
+        
+        <h2>Deja que nuestras cartas te guien</h2>
+        <Form/>
+        <Button primary text={'Ir a la tirada'}/>
+      </section>
+      <aside>
+        <img  className={styles.heroImg} src="/img/hero-img.png" alt="imagen tres cartas" />
+      </aside>
     </main>
   );
 }
