3d51791758517d21bf1a40f1ab5d11e5997d661f
GabriiCoder <150010585+GabriiCoder@users.noreply.github.com>
2024-01-23 12:51:35 +0100

    añadi algo al componente Card

--- src/app/tirada/page.jsx
+++ src/app/tirada/page.jsx
@@ -2,14 +2,16 @@
 import styles from './page.module.css';
 import Carousel from '../components/Carousel/Carousel';
 import { useCards } from '@/context/cardContext';
+import Card from '../components/Card/Card';
 
 export default function Tirada() {
     const cards = useCards();
     return (
-    
+
       <main className={styles.main}>
         <Carousel cards={cards} />
-      
+      < Card />
       </main>
+      
   )
 }
